a94e17ebb68d0ce4b49fcd35c87a7154c997f6c7
Nitishupkr <95420063+Nitishupkr@users.noreply.github.com>
2024-06-20 12:52:33 +0530

    Update page.jsx

--- pizza-rasoi-main/src/app/page.jsx
+++ pizza-rasoi-main/src/app/page.jsx
@@ -11,9 +11,9 @@ export default function Home() {
       <section className="text-center my-16" id="about">
         <SectionHeaders subHeader={'Our story'} mainHeader={'About us'} />
         <div className="text-gray-500 max-w-md mx-auto mt-4 flex flex-col gap-4">
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. A illum iure perferendis ipsum excepturi, tenetur repellendus placeat provident nam adipisci facilis asperiores. Ex eos numquam nobis ipsam cumque? Minus, consequuntur</p>
-          <p>iure perferendis ipsum excepturi, tenetur repellendus placeat provident nam adipisci facilis asperiores. Ex eos numquam nobis ipsam cumque Minus, consequuntur</p>
-          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa iusto doloremque cumque adipisci quasi placeat doloribus!</p>
+          <p>At Pizza Rasoi, we believe that every pizza should be a delightful experience, crafted with passion and served with love. Our journey began with a simple idea: to bring the authentic taste of freshly made pizzas right to your doorstep. We are dedicated to using only the finest ingredients to create mouth-watering pizzas that leave you craving for more.</p>
+          <p>Pizza Rasoi was founded by a team of food enthusiasts who share a deep love for pizza. Our mission is to redefine the pizza ordering experience by combining traditional recipes with modern technology. We understand the joy of biting into a perfectly baked pizza with a crispy crust, rich tomato sauce, and melted cheese. That's why we go the extra mile to ensure that every pizza we deliver meets our high standards of quality and taste.</p>
+      
         </div>
       </section>
       <section className="text-center my-8" id="contact">
